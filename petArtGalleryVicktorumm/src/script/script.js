c59e8d07130740f2410f3f0b12cd56589140852c
Dmitry <kererelore14@gmail.com>
2026-03-31 15:09:45 +0300

    fix: convert submodule to regular folder

---
+++ petArtGalleryVicktorumm/src/script/script.js
@@ -0,0 +1,260 @@
+'use strict';
+
+window.addEventListener('DOMContentLoaded', () => {
+
+    const selectLang = document.querySelector('.header-swap-language'),
+        header = document.querySelector('.header__wrapper'),
+        basketTrigger = header.querySelector('.bascet-trigger'),
+        basketShow = document.querySelector('.shop__basket-show-parent'),
+        basketClose = document.querySelector('.shop__basket-back'),
+        artParent = document.querySelector('.shop'),
+        basketMain = document.querySelector('.shop__basket-main'),
+        basketInfo = basketMain.querySelector('.shop__basket-orders'),
+        basketItemContainer = document.querySelector('.shop__basket-order-items'),
+        basketTotal = document.querySelector('.shop__basket-total'),
+        basketTotalTable = basketTotal.querySelector('.total-table'),
+        basketNoItems = document.querySelector('.bascet-zero-item'),
+        basketClear = document.querySelector('.shop__basket-button');
+
+    const basketMainDb = new Map();
+    let lang = selectLang.value;
+    swapLang(lang)
+    BasketRender(); 
+    getBasketItems()
+    artParent.innerHTML = '';
+
+    selectLang.addEventListener('change', () => {
+        lang = selectLang.value;
+        swapLang(lang)
+    })
+
+    class Art {
+        constructor(id, title, artSize, paintType, paintTypeUa, year, img, priceUSD, priceUAH) {
+            this.id = id;
+            this.title = title;
+            this.artSize = artSize;
+            this.paintType = paintType;
+            this.paintTypeUa = paintTypeUa;
+            this.year = year;
+            this.img = img;
+            this.priceUSD = priceUSD;
+            this.priceUAH = priceUAH;
+        }   
+
+        render() {
+
+            const item = document.createElement('div');
+            item.classList.add('shop-item')
+            item.innerHTML += `
+            <div class="shop-item-view">
+              <img src="assets/prod/${this.img}" alt="art" class="shop-item-view-img">
+              <img src="assets/icons/shop-slider-bar.svg" alt="" class="shop-item-view-slider-bar">
+              <p class="shop-item-price en">${this.priceUSD}$</p>
+              <p class="shop-item-price uk hide" style="font-weight: 600;  ">${this.priceUAH} грн</p>
+            </div>
+            <div class="shop-item-info">
+            <p class="item-info-title">"${this.title}"</p>
+            <ul class="item-info-list">
+              <li class="item-info-list-el">${this.artSize}</li>
+              <li class="item-info-list-el en">${this.paintType}</li>
+              <li class="item-info-list-el uk hide" style="font-weight: 600;">${this.paintTypeUa}</li>
+              <li class="item-info-list-el">${this.year}</li>
+            </ul>
+            <button class="btn shop-item-btn">Buy</button>
+          </div>
+            `;
+            artParent.append(item)
+            this.buyButton = item.querySelector('.shop-item-btn')
+            item.addEventListener('click', (e) => {
+                if (e.target === this.buyButton) {
+                    // basketMainDb.set(this.id, {
+                    //     title: this.title,
+                    //     img: this.img,
+                    //     priceUSD: this.priceUSD,
+                    //     priceUAH: this.priceUAH,
+                    //     id: this.id
+                    // });
+                    addToBasket({
+                        id: this.id, 
+                        title: this.title,
+                        img: this.img,
+                        priceUSD: this.priceUSD,
+                        priceUAH: this.priceUAH,
+                    }).then(getBasketItems)
+                    
+
+                }
+            })
+
+        }
+    }
+
+    async function addToBasket(item){
+        await fetch('http://localhost:3000/basket', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(item)
+        })
+    }
+
+    async function getBasketItems(){
+        const res = await fetch('http://localhost:3000/basket')
+        const data = await res.json();
+        basketMainDb.clear()
+        data.forEach(item => {
+            basketMainDb.set(item.id, item)
+        })
+        BasketRender()
+    }
+
+    async function deleteItemOfBasket(id){
+        -
+         
+        await fetch(`http://localhost:3000/basket/${id}`, {
+            method: 'DELETE'
+        });
+        getBasketItems();
+    }
+
+    async function deleteItemOfBasketAll(){
+        const res = await fetch('http://localhost:3000/basket')
+        const items = await res.json()
+        
+        items.forEach((el) => {
+            deleteItemOfBasket(el.id)
+        })
+        
+        getBasketItems();
+    }
+
+    const getArtCards = async (url) => {
+        const res = await fetch(url)
+        if(!res.ok){
+            throw new Error(`Couldnt fetch are product's of ${url}, error:${res.status}`)
+        }
+        return await res.json()
+    }
+
+
+    getArtCards('http://localhost:3000/artDB')
+    .then(data => data.forEach(({id, title, artSize, paintType, paintTypeUa, year, img, priceUSD, priceUAH}) => {
+        new Art(id, title, artSize, paintType, paintTypeUa, year, img, priceUSD, priceUAH).render();
+    }))
+
+    function BasketRender() {
+        if (basketMainDb.size === 0) {
+            basketNoItems.classList.remove('hide');
+            basketTotal.classList.add('hide')
+            basketInfo.classList.add('hide')
+        } else {
+            basketNoItems.classList.add('hide');
+            basketTotal.classList.remove('hide')
+            basketInfo.classList.remove('hide')
+            basketItemContainer.innerHTML = '';
+            basketTotalTable.innerHTML = '';
+            let totalPriceUSD = 0
+            let totalPriceUAH = 0
+
+            for (const { title, img, priceUSD, priceUAH, id} of basketMainDb.values()) {
+                totalPriceUSD += priceUSD
+                totalPriceUAH += priceUAH
+                const item = document.createElement('div');
+                item.classList.add('order-item')
+                item.innerHTML += `
+        <div class="order-item-title" data-delete="${id}">
+            <p class="order-item-title-descroption">"${title}"</p>
+            <img src="assets/icons/order-item-delete.svg" alt="close-icon" class="order-item-title-delete" data-delete>
+        </div>
+        <img src="assets/prod/${img}" alt="art" class="order-item-view">
+        `;
+                const tr = document.createElement('tr')
+                tr.innerHTML += `
+                <td>
+                    <p class="order-item-title-descroption">"${title}" :</p>
+                </td>
+                <td>
+                    <p class="order-item-price en">${priceUSD}$</p>
+                    <p class="order-item-price uk hide">${priceUAH} грн.</p>
+                </td>
+            `;
+                basketTotalTable.append(tr)
+                basketItemContainer.append(item)
+            }
+
+            basketTotalTable.innerHTML += `
+             <tr>
+                        <td>
+                            <p class="order-title en">To pay:</p>
+                            <p class="order-title uk hide">До сплати:</p>
+                        </td>
+                        <td>
+                            <p class="order-item-price-all en">${totalPriceUSD}$</p>
+                            <p class="order-item-price-all uk hide">${totalPriceUAH} грн.</p>
+                        </td>
+                    </tr>
+            `;
+            
+        }
+        swapLang(lang);
+    }
+
+    header.addEventListener('click', (e) => {
+        if (e.target === basketTrigger) {
+            basketShow.classList.toggle('hide')
+        }
+    })
+
+    basketShow.addEventListener('click', (e) => {
+        if (e.target === basketClose) {
+            basketShow.classList.toggle('hide')
+        } else if (e.target === basketClear) {
+            if (basketMainDb.size > 0) {
+                
+                const confirmThis = document.querySelector('.confirm')
+                confirmThis.classList.remove('hide')
+                confirmThis.addEventListener('click', (event) => {
+                    if (event.target.classList.contains('conf')) {
+                        confirmThis.classList.add('hide')
+                        basketMainDb.clear();
+                        deleteItemOfBasketAll()
+                        BasketRender();
+                    } else {
+                        confirmThis.classList.add('hide')
+                    }
+                }, {once: true})
+            } else {
+                return;
+            }
+        } else if (e.target === document.querySelector('.bascet-zero-btn')) {
+            basketShow.classList.toggle('hide');
+        } else if(e.target.classList.contains('order-item-title-delete')){
+                    let id = Number(e.target.closest('.order-item-title').dataset.delete)
+                    deleteItemOfBasket(id)
+        }
+    })
+
+    function swapLang(currentLang) {
+        const allEnItems = document.querySelectorAll('.en'),
+            allUkItems = document.querySelectorAll('.uk');
+        if (currentLang === 'en') {
+            allEnItems.forEach(item => {
+                item.classList.remove('hide')
+            });
+            allUkItems.forEach(item => {
+                item.classList.add('hide')
+            });
+        } else if (currentLang === 'uk') {
+            allUkItems.forEach(item => {
+                item.classList.remove('hide')
+                if (!item.classList.contains('about__description')) {
+                    item.style.fontFamily = 'Montserrat'
+                }
+            });
+            allEnItems.forEach(item => {
+                item.classList.add('hide')
+            });
+        }
+    }
+})
